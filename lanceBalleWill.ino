a170e1813c20e1ab9c7905283df4b1244262ea5f
William Guimont-Martin <wil9876543210@hotmail.com>
2021-10-03 23:16:19 -0400

    reformat code

--- lanceBalleWill.ino
+++ lanceBalleWill.ino
@@ -12,31 +12,36 @@ const int ledPin = 13;
 const int readyAngle = 35;
 const int pushAngle = 120;
 
-class Launcher {
+class Launcher
+{
 public:
-  Launcher(int pin1, int pin2, int pin3) : pin1(pin1), 
-                                           pin2(pin2), 
-                                           pin3(pin3) {};
+  Launcher(int pin1, int pin2, int pin3) : pin1(pin1),
+                                           pin2(pin2),
+                                           pin3(pin3){};
 
-  void forward() {
+  void forward()
+  {
     digitalWrite(pin1, HIGH);
     digitalWrite(pin2, LOW);
-    for (int i = 0; i < 255; ++i) {
+    for (int i = 0; i < 255; ++i)
+    {
       analogWrite(pin3, i);
       delay(10);
     }
   }
 
-  void backward() {
+  void backward()
+  {
     digitalWrite(pin1, LOW);
     digitalWrite(pin2, HIGH);
   }
 
-  void stop() {
+  void stop()
+  {
     digitalWrite(pin1, LOW);
     digitalWrite(pin2, LOW);
   }
-  
+
 private:
   int pin1;
   int pin2;
@@ -47,17 +52,18 @@ Launcher right(pinA1, pinA2, pinA3);
 Launcher left(pinB1, pinB2, pinB3);
 Servo feeder;
 
-void setup() {
+void setup()
+{
   pinMode(pinA1, OUTPUT);
   pinMode(pinA2, OUTPUT);
   pinMode(pinA3, OUTPUT);
   pinMode(pinB1, OUTPUT);
   pinMode(pinB2, OUTPUT);
   pinMode(pinB3, OUTPUT);
-  
+
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
-  
+
   feeder.attach(5);
   feeder.write(pushAngle);
 
@@ -65,20 +71,23 @@ void setup() {
   right.forward();
 }
 
-void loop() {
+void loop()
+{
   int val = digitalRead(buttonPin);
-  if (val == HIGH) {
+  if (val == HIGH)
+  {
     // pressed
     digitalWrite(ledPin, LOW);
     feeder.write(readyAngle);
     delay(100);
-    feeder.write(readyAngle+10);
+    feeder.write(readyAngle + 10);
     delay(100);
     feeder.write(readyAngle);
     delay(100);
     feeder.write(pushAngle);
-    
-  } else {
+  }
+  else
+  {
     // not pressed
     digitalWrite(ledPin, HIGH);
   }
